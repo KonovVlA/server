5940a6d8b648e9358ce8f1294df97e4e018be188
Konov.Vl.A <Konov.Vl.A@yandex.ru>
2025-02-02 13:12:57 +0300

    test1

--- src/main/java/ru/kva/controllers/TestController.java
+++ src/main/java/ru/kva/controllers/TestController.java
@@ -1,16 +1,48 @@
 package ru.kva.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "<div style=\"display: flex;flex-direction: column;gap: 3rem;padding: 2rem;font-size: 24px;\">" +
-                "<div>Открытый паевой инвестиционный фонд (ОПИФ) — это форма коллективного инвестирования, при котором средства участников (пайщиков) объединяются под управлением профессиональной управляющей компании для вложения в ценные бумаги, недвижимость, сырьевые активы и другие инструменты</div>" +
-                "<div>Закрытый паевой инвестиционный фонд (ЗПИФ) — это форма коллективного инвестирования, где средства пайщиков объединяются для финансирования долгосрочных проектов (например, строительства, венчурных стартапов, инфраструктуры). В отличие от открытых фондов, паи ЗПИФа нельзя свободно купить или погасить в любой момент — это возможно только в определенные сроки или при завершении работы фонда</div>" +
-                "</div>";
+    private static final List<String> rq = new ArrayList<>();
+    private static final Map<String, Object> rs = new HashMap<>();
+
+    @PostMapping("/put-url")
+    public Object putUrl(@RequestBody Map<String, String> body) {
+        var url = body.get("url");
+        rq.add(url);
+        while (true) {
+            if (rs.get(url) != null) {
+                var ttt = rs.get(url);
+                rs.remove(url);
+                return ttt;
+            }
+        }
     }
+
+    @GetMapping("/get-url")
+    public String getUrl() {
+        if (rq.isEmpty()) {
+            return null;
+        }
+        String result = rq.get(0);
+        rq.remove(0);
+        return result;
+    }
+
+    @PostMapping("/put-dep")
+    public String putDep(@RequestBody Map<String, Object> body) {
+        rs.put(body.get("url").toString(), body);
+        return "OK";
+    }
+
 }
